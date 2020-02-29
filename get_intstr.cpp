7d91a99b66bc40f33085352f7894f3d6e3520649
hongyi peng <Homephy@outlook.com>
2020-02-29 11:29:21 +0800

    three

---
+++ get_intstr.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//#include<cstdlib>
+//
+//using namespace std;
+//int main(void) {
+//	const int Arsize = 20;
+//	char name[Arsize];
+//	char dessert[Arsize];
+//
+//	cout << "Enter your name:\n";
+//	cin.get(name, Arsize).get();     //read string ,and get a newline
+//	cout << "Enter your favorite dessert:\n";
+//	cin.get(dessert, Arsize).get();
+//	cout << "I have some delicious " << dessert;
+//	cout << " for you, " << name << endl;
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ getline_intstr.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//#include<cstdlib>
+//
+//using namespace std;
+//int main(void) {
+//	const int Arsize = 20;
+//	char name[Arsize];
+//	char dessert[Arsize];
+//
+//	cout << "Enter your name:\n";
+//	cin.getline(name, Arsize);     //read through newline
+//	cout << "Enter your favorite dessert:\n";
+//	cin.getline(dessert, Arsize);
+//	cout << "I have some delicious " << dessert;
+//	cout << "for you, " << name << endl;
+//
+//	system("pause");
+//	return 0;
+//}
