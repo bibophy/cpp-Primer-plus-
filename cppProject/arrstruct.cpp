e7562ae15b5f76020b9f3119c02fe9831371510a
hongyi peng <Homephy@outlook.com>
2020-03-03 19:12:13 +0800

    cppproject

---
+++ cppProject/arrstruct.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//
+//using std::cout;
+//using std::cin;
+//using std::endl;
+//using std::ios;
+//
+//#include <iomanip>
+//
+//using std::setiosflags;
+//using std::setprecision;
+//
+//int main()
+//{
+//    /* declare program variables here */
+//    int hours;
+//    long int salary;
+//    cout << "Enter hours worked (-1 to end): "
+//        << setiosflags(ios::fixed | ios::showpoint);
+//    cin >> hours;
+//
+//    /* write code to begin loop here */ {
+//        /* write code here to prompt and input hourly rate */
+//
+//        /* write code here to determine if hours worked are less
+//           than or equal to 40 and if so, calculate base pay.
+//           If not, calculate base + overtime pay */
+//        cout << "Salary is $" << setprecision(2);
+//        cin >> salary;
+//        cout << "\n\nEnter hours worked (-1 to end): ";
+//        cin >> hours;
+//    }
+//
+//    return 0;
+//}
---
+++ cppProject/混合输入字符串和数字.cpp
@@ -0,0 +1,18 @@
+//#include<iostream>
+//#include<cstdlib>
+//
+//using namespace std;
+//int main(void) {
+//	cout << "What year was your house bulit?\n";
+//	int year;
+//	(cin >> year).get();
+//	cout << "What is its street address?\n";
+//	char address[80];
+//	cin.getline(address, 80);
+//	cout << "Year bulit:" << year << endl;
+//	cout << "Address:" << address << endl;
+//
+//	system("pause");
+//	return 0;
+//
+//}
