6cb31bf24bbb16abdee150caf8579b433bcf7a53
hongyi peng <Homephy@outlook.com>
2020-03-12 18:15:03 +0800

    对象与类

---
+++ cppProject/字符串倒装.cpp
@@ -0,0 +1,15 @@
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//int main() {
+//    cout << "Enter your word:";
+//    string word;
+//    cin >> word;
+//
+//    for (int i = word.size() - 1; i >= 0; i--)   //string类的size()获得字符串的数
+//        cout << word[i];
+//    cout << "\nBye";
+//
+//    return 0;
+// }
